bd3d33c3eb579ea8506fd5bbe6dfd54234ff3648
Daniel Gomez <danielgomezaristizabal2020@gmail.com>
2023-08-15 22:29:05 -0500

    s

---
+++ Amortizacion.py
@@ -0,0 +1,37 @@
+import CreditCardTests
+
+def Total_Interest(monto,tasa,cuotas):
+    valor_cuota = CreditCardTests.CalculateFee(monto, tasa, cuotas)
+    total_intereses = (valor_cuota * cuotas) - monto
+    return total_intereses
+
+def Amortizacion(monto, tasa, cuotas):
+    valor_cuota = CreditCardTests.CalculateFee(monto, tasa, cuotas)
+    print(valor_cuota)
+    interes_x = tasa/100
+    saldo = monto
+    tabla_amortizacion = [["Cuota", "Saldo", "Pago interés", "Abono capital"], ["#", valor_cuota, tasa, monto]]
+    if cuotas == 1:
+        numero_cuota = 1
+        interes = (tasa * saldo) / 100
+        abono_capital = valor_cuota - interes
+        fila = [numero_cuota, saldo, interes, abono_capital]
+        tabla_amortizacion.append(fila)
+    else:
+        for cuota in range(1, cuotas + 1):
+            numero_cuota = cuota
+            interes = interes_x * saldo
+            abono_capital = valor_cuota - interes
+            saldo = saldo - abono_capital
+            
+            fila = [numero_cuota, saldo, interes, abono_capital]
+            tabla_amortizacion.append(fila)
+            print(fila)
+
+    return tabla_amortizacion
+
+monto = 8500002
+cuotas = 25
+tasa_interes = 3.7
+
+print(Amortizacion(monto, tasa_interes, cuotas))

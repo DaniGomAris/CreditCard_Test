8f6ed7fe5774855adc5e3f97e6b75b0c737d217b
Daniel Gomez Aristizabal <99361024+DaniGomAris@users.noreply.github.com>
2023-08-18 07:42:42 -0500

    Create AmortizacionTests.py

---
+++ AmortizacionTests.py
@@ -0,0 +1,19 @@
+import unittest
+import Amortizacion
+
+class AmortizacionTests(unittest.TestCase):
+
+    def Cuota_unica(self):
+        pass
+
+    def Caso_normal(self):
+        pass
+
+    def Caso_normal2(self):
+        pass
+
+    def Tasa_cerp(self):
+        pass
+
+    def Usura(self):
+        pass

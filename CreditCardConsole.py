3107ef366acb16ac86991c06917eaff54b63a4a4
Daniel Gomez <danielgomezaristizabal2020@gmail.com>
2023-08-19 11:25:30 -0500

    Final commit

--- CreditCardConsole.py
+++
@@ -1,8 +0,0 @@
-import Payment
-
-print("Este programa permite calcular la cuota a pagar por una compra con tarjeta de credito")
-monto = float(input("Monto de la compra: "))
-tasa = float(input("Interes de la tarjeta: "))
-cuotas = float(input("Numero de cuotas en que va a inferir la compra: "))
-
-print((Payment.CalculateFee(monto,tasa,cuotas)))

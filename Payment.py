8b3946976e3031bbf1fd236c6dfbce1aa61072a2
Daniel Gomez Aristizabal <99361024+DaniGomAris@users.noreply.github.com>
2023-08-30 07:19:04 -0500

    Update Payment.py
    Payment correction

--- Payment.py
+++ Payment.py
@@ -1,16 +1,19 @@
 import Exceptions
+from abc import ABC
 
-def calculateFee(monto,tasa,cuotas):
-    p =  tasa / 100
-    if monto == 0:
-        raise Exceptions.ZeroAmount("El monto es 0")
-    if tasa*12 > 100 :
-        raise Exceptions.ExcessiveRate("La tasa no debe ser superior a 100")
-    if cuotas <= 0:
-        raise Exceptions.NegativeShare("Las cuotas son menores o iguales a 0")
-    if cuotas == 1 :
-        return monto
-    if tasa == 0:
-        return monto / cuotas
-    else:         
-        return (monto * p) / (1 - (1 + p) ** (-cuotas))
+class CreditCardPayment(ABC):
+    
+    def calculateFee(monto,tasa,cuotas):
+        p =  tasa / 100
+        if monto == 0:
+            raise Exceptions.ZeroAmount("El monto es 0")
+        if tasa*12 > 100 :
+            raise Exceptions.ExcessiveRate("La tasa no debe ser superior a 100")
+        if cuotas <= 0:
+            raise Exceptions.NegativeShare("Las cuotas son menores o iguales a 0")
+        if cuotas == 1 :
+            return monto
+        if tasa == 0:
+            return monto / cuotas
+        else:         
+            return (monto * p) / (1 - (1 + p) ** (-cuotas))

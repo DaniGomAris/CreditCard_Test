fde5961d90effab19cedb8ea7f8a0824d3fd9deb
Daniel Gomez Aristizabal <99361024+DaniGomAris@users.noreply.github.com>
2023-08-30 07:18:23 -0500

    Update Menu.py
    Menu Correction

--- Menu.py
+++ Menu.py
@@ -35,14 +35,14 @@ Menu
         monto = float(input("Monto a pagar: "))
         tasa = float(input("Tasa actual: "))
         cuotas = int(input("Cúantas cuotas pagará el monto: "))
-        print(f"Total interests: {Payment.calculateFee(monto,tasa,cuotas)}")
+        print(f"Total interests: {Payment.CreditCardPayment.calculateFee(monto,tasa,cuotas)}")
         self.election_option()
         
     def amortizacion_option(self):
         monto = float(input("Monto a pagar: "))
         tasa = float(input("Tasa actual: "))
         cuotas = int(input("Cúantas cuotas pagará el monto: "))
-        print(Amortizacion.amortizacion(monto,tasa,cuotas))
+        print(Amortizacion.CreditCardAmortizacion.amortizacion(monto,tasa,cuotas))
         self.election_option()
         
     def extra_bonus_option(self):
@@ -51,5 +51,5 @@ Menu
         cuotas = int(input("Cúantas cuotas pagará el monto: "))
         numero_cuota_a_abonar = int(input("Qué cuota va a hacer un abono extra: "))
         abonoextra = float(input("Cúanto será el abono extra: "))
-        print(ExtraBonus.amortizacion_extra_bonus(monto,tasa,cuotas,numero_cuota_a_abonar,abonoextra))
+        print(ExtraBonus.CreditCardExtraBonus.extra_bonus(monto,tasa,cuotas,numero_cuota_a_abonar,abonoextra))
         self.election_option()

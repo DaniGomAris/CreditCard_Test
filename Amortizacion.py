e9d9889c683057f357d45af3ac2a841fa4761ccd
Daniel Gomez Aristizabal <99361024+DaniGomAris@users.noreply.github.com>
2023-09-01 07:35:42 -0500

    Update Amortizacion.py

--- Amortizacion.py
+++ Amortizacion.py
@@ -3,31 +3,31 @@ from abc import ABC
 
 class CreditCardAmortizacion(ABC):
 
-    def total_Interest(monto,tasa,cuotas):
-        valor_cuota = Payment.CreditCardPayment.calculateFee(monto, tasa, cuotas)
-        total_intereses = (valor_cuota * cuotas) - monto
-        return total_intereses
+    def total_Interest(amount: float, rate: float ,pay: int):
+        payment_value = Payment.CreditCardPayment.calculateFee(amount, rate, pay)
+        total_interests = (payment_value * pay) - amount
+        return total_interests
 
-    def amortizacion(monto, tasa, cuotas):
-        valor_cuota = Payment.CreditCardPayment.calculateFee(monto, tasa, cuotas)
-        print(valor_cuota)
-        interes_x = tasa/100
-        saldo = monto
-        tabla_amortizacion = [["Cuota", "Saldo", "Pago interés", "Abono capital"], ["#", valor_cuota, tasa, monto]]
-        if cuotas == 1:
-            numero_cuota = 1
-            interes = (tasa * saldo) / 100
-            abono_capital = valor_cuota - interes
-            fila = [numero_cuota, saldo, interes, abono_capital]
-            tabla_amortizacion.append(fila)
+    def amortizacion(amount: float, rate: float ,pay: int):
+        payment_value = Payment.CreditCardPayment.calculateFee(amount, rate, pay)
+        print(payment_value)
+        interests_x = rate/100
+        balance = amount
+        amortizacion_table = [["payment", "balance", "Interest pay", "Capital bonus"], ["#", payment_value, rate, amount]]
+
+        if pay == 1:
+            payment_number = 1
+            interests = (rate * balance) / 100
+            capital_bonus = payment_value - interests
+            row = [payment_number, balance, interests, capital_bonus]
+            amortizacion_table.append(row)
         else:
-            for cuota in range(1, cuotas + 1):
-                numero_cuota = cuota
-                interes = interes_x * saldo
-                abono_capital = valor_cuota - interes
-                saldo = saldo - abono_capital
-                fila = [numero_cuota, saldo, interes, abono_capital]
-                tabla_amortizacion.append(fila)
-                print(fila)
-
-        return tabla_amortizacion
+            for payment in range(1, pay + 1):
+                payment_number = payment
+                interests = interests_x * balance
+                capital_bonus = payment_value - interests
+                balance = balance - capital_bonus
+                row = [payment_number, balance, interests, capital_bonus]
+                amortizacion_table.append(row)
+                print(row)
+        return amortizacion_table

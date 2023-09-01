97b3d1e7502f7aedd9ba5f171dd601cd9e6f16d6
Daniel Gomez Aristizabal <99361024+DaniGomAris@users.noreply.github.com>
2023-09-01 07:37:03 -0500

    Update ExtraBonusTests.py

--- ExtraBonusTests.py
+++ ExtraBonusTests.py
@@ -1,37 +1,46 @@
 import unittest
 import ExtraBonus
-import Payment
 import Exceptions
 
 class BonusEffectTest(unittest.TestCase):
 
     def normal_case(self):
-        monto = 200000
-        tasa = 3.1
-        cuotas = 36
-        numero_cuota_a_abonar = 10
-        abonoextra = 53000
+        amount = 200000
+        rate = 3.1
+        pay = 36
+        number_pay_of_bonus = 10
+        extrabonus = 53000
+        result = ExtraBonus.CreditCardExtraBonus.extra_bonus(amount, rate, pay, number_pay_of_bonus, extrabonus)
+
+        self.assertEqual(pay, round(result,2))
 
     def normal_case_2(self):
-        monto = 850000
-        tasa = 3.4
-        cuotas = 24
-        numero_cuota_a_abonar = 5
-        abonoextra = 90000
+        amount = 850000
+        rate = 3.4
+        pay = 24
+        number_pay_of_bonus = 5
+        extrabonus = 90000
+        result = ExtraBonus.CreditCardExtraBonus.extra_bonus(amount, rate, pay, number_pay_of_bonus, extrabonus)
+
+        self.assertEqual(pay, round(result,2))
 
     def low_bonus_error(self):
-        monto = 850000
-        tasa = 3.4
-        cuotas = 24
-        numero_cuota_a_abonar = 10
-        abonoextra = 45000
+        amount = 850000
+        rate = 3.4
+        pay = 24
+        number_pay_of_bonus = 10
+        extrabonus = 45000
+
+        self.assertRaises(Exceptions.LowBonus, ExtraBonus.CreditCardExtraBonus.extra_bonus, amount, rate, pay, number_pay_of_bonus, extrabonus)
 
     def late_fee_error(self):
-        monto = 850000
-        tasa = 3.4
-        cuotas = 24
-        numero_cuota_a_abonar = 22
-        abonoextra = 180000
+        amount = 850000
+        rate = 3.4
+        pay = 24
+        number_pay_of_bonus = 22
+        extrabonus = 180000
 
+        self.assertRaises(Exceptions.Latefee, ExtraBonus.CreditCardExtraBonus.extra_bonus, amount, rate, pay, number_pay_of_bonus, extrabonus)
+        
 if __name__ == '__main__':
     unittest.main()
